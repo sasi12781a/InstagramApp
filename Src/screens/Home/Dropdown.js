ede7dfc23910581f723d3da3adfe9e8638777f28
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-07-26 11:30:22 +0530

    m

---
+++ Src/screens/Home/Dropdown.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+function DropDown() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
+
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+    />
+  );
+}
+
+export default DropDown;
