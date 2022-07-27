99d07b28688b34cde2ee2899c3799e9f5acffc31
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-07-27 11:03:54 +0530

    m

---
+++ Src/screens/Home/ModalBox.js
@@ -0,0 +1,57 @@
+import React , {useState} from 'react';
+import { StyleSheet, View , Text , Modal , Button } from 'react-native';
+export default function App() {
+    const [active , setactive] = useState(false);
+    return (
+        <View style={styles.container}>
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={active}
+            onRequestClose={() => {
+            console.warn("closed");
+            }}
+            >
+            <View style={styles.container}>
+                <View style={styles.View}>
+                <Text style={styles.text}>GeeksforGeeks</Text>
+                <Button title="close"
+                        onPress={()=>{setactive(!active)}}/>
+                </View>
+            </View>
+            </Modal>
+            <Button
+            title={"click"}
+            onPress={()=>{setactive(!active)}}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+container: {
+	flex: 1,
+	backgroundColor : "black",
+	alignItems: 'center',
+	justifyContent: 'center',
+},
+View : {
+	backgroundColor : "white" ,
+	height : 140 ,
+	width : 250,
+	borderRadius : 15,
+	alignItems : "center",
+	justifyContent : "center",
+	borderColor : "black",
+	borderWidth:2,
+},
+text : {
+	fontSize : 20,
+	color : "green",
+	marginBottom:20
+},
+button : {
+	margin : 20,
+	width:200,
+}
+});
